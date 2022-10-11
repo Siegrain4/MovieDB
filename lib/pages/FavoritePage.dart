00821fd8b341bec1cac1488bae53f7ef52da9615
Siegrain4 <reykazuko@gmail.com>
2022-10-11 13:28:11 +0700

    Menambahkan isi Tab Favorite pada project

--- lib/pages/FavoritePage.dart
+++ lib/pages/FavoritePage.dart
@@ -1,10 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:moviedb/widgets/CustomNavBar.dart';
+import 'package:moviedb/widgets/MoviePageButtons.dart';
+import 'package:moviedb/widgets/RecommendWidget.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.4,
+            child: Image.asset(
+              "images/cover.jpg",
+              height: 280,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.arrow_back,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.favorite_border,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 60),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blue.withOpacity(0.5),
+                                  spreadRadius: 1,
+                                  blurRadius: 8,
+                                )
+                              ]),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              'images/8.jpg',
+                              height: 250,
+                              width: 180,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 50, top: 70),
+                          height: 80,
+                          width: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(40),
+                            color: Colors.blue,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.blue.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 8,
+                              ),
+                            ],
+                          ),
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  MoviePageButtons(),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Doctor Strange 2",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Hello Friends, in this video I will teach you how to design movie app ui, movie app ui design, movies app design, movies app ui design, movie app ui ux design, movies app ui ux design, movie app ui kit, movie theatre ui design.",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                          textAlign: TextAlign.justify,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  RecommendWidget(),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: CustomNavBar(),
+    );
   }
 }
